511436ae7fbb5321227619793f0e03f8df91e8d4
Levan Dalbashvili <levanidalbashvili00@gmail.com>
2024-05-01 19:42:16 +0700

    done footer desgin with links

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,25 +1,5 @@
 <footer class="bg-white">
     <div class="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
-        <nav class="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12" aria-label="Footer">
-            <div class="pb-6">
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">About</a>
-            </div>
-            <div class="pb-6">
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Blog</a>
-            </div>
-            <div class="pb-6">
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Jobs</a>
-            </div>
-            <div class="pb-6">
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Press</a>
-            </div>
-            <div class="pb-6">
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Accessibility</a>
-            </div>
-            <div class="pb-6">
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Partners</a>
-            </div>
-        </nav>
         <div class="mt-10 flex justify-center space-x-10">
             <a href="https://www.facebook.com/levani.dalbashvili.7" class="text-gray-400 hover:text-gray-500">
                 <span class="sr-only">Facebook</span>
@@ -46,6 +26,6 @@
                 </svg>
             </a>
         </div>
-        <p class="mt-10 text-center text-xs leading-5 text-gray-500">&copy; 2020 Your Company, Inc. All rights reserved.</p>
+        <p class="mt-10 text-center text-xs leading-5 text-gray-500">&copy; 2024 Levan Dalbashvili</p>
     </div>
 </footer>
