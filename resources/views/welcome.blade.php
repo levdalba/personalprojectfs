e1da86486ddcaee1e32bbc542e3a3b393e38857e
Levan Dalbashvili <levanidalbashvili00@gmail.com>
2024-05-02 01:22:07 +0700

    design of welcome page finished

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,13 @@
-<x-site-layout title="Welcome To My Personal Portfolio">
-   
-    <div class="flex items-center justify-center min-h-screen bg-gradient-to-r from-slate-500 to-neutral-500">
-    <div class="text-center p-8 bg-white shadow-xl rounded-lg">
-        <h1 class="text-4xl font-bold text-gray-800 mb-3">Welcome, I'm Levan Dalbashvili</h1>
-        <p class="text-gray-600 text-lg mb-6">Developer | Competetive Programmer | Student</p>
-        <a href="/aboutme" class="inline-block bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out">
-            Discover My Work
-        </a>
+<x-site-layout title="Welcome To My Personal Website">
+    <div class="flex items-center justify-center min-h-screen bg-gradient-to-bl from-grey-500 via-slate-600 to-purple-700">
+        <div class="text-center p-10 bg-white/80 backdrop-blur-md shadow-xl rounded-xl transition-all duration-300 ease-in-out hover:bg-white/90">
+            <h1 class="text-5xl font-bold text-gray-800 mb-3 animate-pulse">Welcome, I'm Levan Dalbashvili</h1>
+            <p class="text-gray-700 text-lg mb-6">Developer | Competitive Programmer | Student</p>
+            <div class="animate-bounce">
+                <a href="/projects" class="inline-block bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out">
+                    Discover My Work
+                </a>
+            </div>
+        </div>
     </div>
-</div>
-
 </x-site-layout>
